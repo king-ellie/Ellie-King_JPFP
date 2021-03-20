a1e94546dd8996fc15b9eb44f23fdd486c25fc6b
Ellie King <elliebking1@gmail.com>
2021-03-20 10:07:59 -0400

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    devtool: 'source-map',
+    entry: path.join(__dirname, './src/index.js'),
+    output: {
+      filename: 'main.js',
+      path: path.join(__dirname, './dist'),
+    },
+    module: {
+      rules: [
+        {
+          loader: 'babel-loader',
+          exclude: /(node_modules)/,
+        },
+      ],
+    },
+  };
