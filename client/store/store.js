1eb703111db7c3c7105dab20d5fa4c567587db31
Ellie King <elliebking1@gmail.com>
2021-03-27 11:42:09 -0400

    created sub-reducers, Tier 1 complete

--- client/store/store.js
+++ client/store/store.js
@@ -1,24 +1,26 @@
-import { createStore } from 'redux'
+import { createStore, combineReducers } from 'redux'
 
 const LOAD_CAMPUSES = 'LOAD_CAMPUSES'
 const LOAD_STUDENTS = 'LOAD_STUDENTS'
 
-const initialState = {
-    campuses: [],
-    students: []
-}
-
-const reducer = (state = initialState, action) => {
+const campusesReducer = (state = [], action) => {
     if (action.type === LOAD_CAMPUSES){
-        return {...state, campuses: action.campuses}
-    }
-    if (action.type === LOAD_STUDENTS){
-        return {...state, students: action.students}
-    }
-    else return state
+        return action.campuses
+    } else return state
 }
 
-const store = createStore(reducer)
+const studentsReducer = (state = [], action) => {
+    if (action.type === LOAD_STUDENTS){
+        return action.students
+    } else return state
+}
+
+const comboReducer = combineReducers ({
+    campuses: campusesReducer,
+    students: studentsReducer
+})
+
+const store = createStore(comboReducer)
 
 export default store
 
